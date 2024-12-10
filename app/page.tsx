6567f41079e522c61899867a1706aa73d426a07c
nordast <nordast@gmail.com>
2024-12-10 13:27:53 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import LandingSections from "@/components/LandingSections";
+
+export default function Chat() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <LandingSections />
+    </div>
+  );
+}
