35053b7cf1e04ea4560b34f5a3c92540a05d97b8
nordast <nordast@gmail.com>
2024-12-10 14:35:11 +0200

    add chat button

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,66 @@
 "use client";
 
 import LandingSections from "@/components/LandingSections";
+import { useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
+import { ArrowDownCircleIcon, MessageCircle } from "lucide-react";
 
 export default function Chat() {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  const [showChatIcon, setShowChatIcon] = useState(false);
+
+  const chatIconRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShowChatIcon(true);
+      } else {
+        setShowChatIcon(false);
+        setIsChatOpen(false);
+      }
+    };
+
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleChat = () => {
+    setIsChatOpen(!isChatOpen);
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       <LandingSections />
+      <AnimatePresence>
+        {showChatIcon && (
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 100 }}
+            transition={{ duration: 0.2 }}
+            className="fixed bottom-4 right-4 z-50"
+          >
+            <Button
+              ref={chatIconRef}
+              onClick={toggleChat}
+              size="icon"
+              className="rounded-full size-14 p-2 shadow-lg"
+            >
+              {!isChatOpen ? (
+                <MessageCircle className="size-12" />
+              ) : (
+                <ArrowDownCircleIcon className="size-12" />
+              )}
+            </Button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
